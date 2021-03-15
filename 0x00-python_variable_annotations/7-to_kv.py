04117d870580e300663d6d6b76f520cf0f1c4abe
kimha1030 <1478@holbertonschool.com>
2021-03-15 15:48:48 -0500

    Complex types string and  int float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv"""
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """[Function that takes a string k and an int OR float v as
+        arguments and returns a tuple]
+
+        Args:
+            k (str): [string used in tuple]
+            v (int or float): [integer or float used in tuple]
+
+        Returns:
+            float: [tuple, where first element is string  and second element is the square of integer o float]
+        """
+    return (k, v*v)
