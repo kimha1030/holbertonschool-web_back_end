621d04569a9e7172fe4abdb1a71121f7ad67adc8
kimha1030 <1478@holbertonschool.com>
2021-03-15 16:42:03 -0500

    Remove Optional of the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Type-annotated function safely_get_value"""
-from typing import TypeVar, Mapping, Any, Union, Optional
+from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
-unionTNone = Union[Optional[T], None]
-unionAnyT = Union[Any, Optional[T]]
+unionTNone = Union[T, None]
+unionAnyT = Union[Any, T]
 
 
-def safely_get_value(dct: Mapping, key: Any, default: unionTNone) -> unionAnyT:
+def safely_get_value(dct: Mapping, key: Any, default: unionTNone = None) -> unionAnyT:
     """ Function safely_get_value"""
     if key in dct:
         return dct[key]
