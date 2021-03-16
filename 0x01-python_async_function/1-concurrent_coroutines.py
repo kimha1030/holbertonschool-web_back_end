57ed8ba8fd17c0331f39fd2ef8d6b5c8e7da55ef
kimha1030 <1478@holbertonschool.com>
2021-03-16 18:01:14 -0500

    Multiple asynchronous coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """[Multiple asynchronous coroutines that return the list of all
         the delays]
-
     Args:
         n (int): [number of iterations used with wait_random].
         max_delay (int): [maximun value of delay].
-
     Returns:
         list: [list of all the delays as float values]
     """
