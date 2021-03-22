c1ec4d1bc972ced59bc418c0d177c1e04f757579
kimha1030 <1478@holbertonschool.com>
2021-03-21 19:39:41 -0500

    lru caching

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class LRUCache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Class LRUCache that inherits from BaseCaching
+        and is a caching system"""
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.list_keys = []
+
+    def put(self, key, item):
+        """Method put"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.list_keys:
+                self.list_keys.append(key)
+            else:
+                self.list_keys.remove(key)
+                self.list_keys.append(key)
+            if len(self.list_keys) > BaseCaching.MAX_ITEMS:
+                leastKeyDel = self.list_keys.pop(0)
+                del self.cache_data[leastKeyDel]
+                print("DISCARD: {}".format(leastKeyDel))
+
+    def get(self, key):
+        """Method get"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
