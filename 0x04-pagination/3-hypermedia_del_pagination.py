d1fe324ba5cc14ab7b446770aa5c4ed259f268a9
kimha1030 <1478@holbertonschool.com>
2021-03-24 17:26:28 -0500

    3-hypermedia_del_pagination.py

---
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ Method that returns a dictionary with the index, data, page_size
+            and next-index"""
+        assert type(index) == int
+        res = self.dataset()
+        assert index >= 0 and index < len(res)
+
+        dict_pages = {}
+        results = index + page_size
+        if results > len(res):
+            next_idx = None
+        else:
+            next_idx = results
+        list_results = []
+        for i in range(index, results):
+            list_results.append(res[i])
+
+        dict_pages["index"] = index
+        dict_pages["data"] = list_results
+        dict_pages["page_size"] = page_size
+        dict_pages["next_index"] = next_idx
+        return dict_pages
