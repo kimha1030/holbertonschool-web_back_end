5fe69133417cd7cf83853b39a56f957082c0c49c
kimha1030 <1478@holbertonschool.com>
2021-03-31 11:59:11 -0500

    Method decode base64 authorization header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 basic_auth.py
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,4 +20,18 @@ class BasicAuth(Auth):
             sec_word = authorization_header.split(' ')[1]
             return sec_word
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Method decode base64 authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            dev = base64.b64decode(base64_authorization_header).decode("utf-8")
+            return dev
+        except Exception:
             return None
