3f9a9b8395ecbed6be1e66019b9af3d3957a0350
kimha1030 <1478@holbertonschool.com>
2021-04-06 18:37:00 -0500

    Fix conditional of before request handler

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -58,11 +58,8 @@ def before_request():
         return
     if not auth.require_auth(request.path, path_list):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None
-    else:
+    if not auth.authorization_header(request) and \
+            not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
