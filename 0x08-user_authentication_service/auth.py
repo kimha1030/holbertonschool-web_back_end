b74027cbaefca12bee7bbc6109048b47072cc5c5
kimha1030 <1478@holbertonschool.com>
2021-04-12 16:53:00 -0500

    Method that returns a user object

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Auth
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -11,3 +14,23 @@ def _hash_password(password: str) -> str:
     p = password.encode('utf-8')
     hashedPassword = bcrypt.hashpw(p, salt)
     return hashedPassword
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Constructor """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method that returns a User object """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            h_password = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=h_password)
+            return user
