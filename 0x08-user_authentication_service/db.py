a611676c1b5f69b8e958c928dd35d6f4fd24ddbd
kimha1030 <1478@holbertonschool.com>
2021-04-12 15:14:52 -0500

    Add method add that returns a User object

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from user import Base
+from user import User
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Method add_user that returns a User object"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
