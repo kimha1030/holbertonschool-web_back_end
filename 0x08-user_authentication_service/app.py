619834cca351d936ebaaae90819a4dff3c2a5769
kimha1030 <1478@holbertonschool.com>
2021-04-12 17:22:58 -0500

    Change importations

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,15 +2,15 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=['GET'])
 def message() -> str:
     """ Method message """
-    return jsonify({"message": "Bienvenue"})
+    return flask.jsonify({"message": "Bienvenue"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
