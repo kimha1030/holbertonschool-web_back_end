75a434d4f3bb54bd86a36884c6435a65c7007aad
kimha1030 <1478@holbertonschool.com>
2021-04-19 12:27:30 -0500

    Method that finds the user corresponding to the email. If the user does not exist, raise a ValueError exception. If it exists, generate a UUID and update the user’s reset_token database field. Return the token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -81,3 +81,15 @@ class Auth:
             return None
         else:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Method that finds the user corresponding to the email.
+            If the user does not exist, raise a ValueError exception.
+            If it exists, generate a UUID and update the user’s
+            reset_token database field. Return the token. """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(user.id, reset_token=_generate_uuid())
+        return user.reset_token
