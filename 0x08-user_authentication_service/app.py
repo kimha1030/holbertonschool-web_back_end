903589340501b999e4c23310996f4c0c56e6ceec
kimha1030 <1478@holbertonschool.com>
2021-04-19 15:08:10 -0500

    Method that allows update the password. If the token is invalid, catch the exception and respond with a 403 HTTP code. Otherwise, respond with a 200 HTTP status and JSON payload

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -83,5 +83,20 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ Method that allows update the password. If the token is invalid,
+        catch the exception and respond with a 403 HTTP code. Otherwise,
+        respond with a 200 HTTP status and JSON payload."""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
