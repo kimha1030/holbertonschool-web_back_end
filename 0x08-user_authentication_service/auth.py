ac841ed1d6534e0c5ce9b3ef78e4e32f026fda79
kimha1030 <1478@holbertonschool.com>
2021-04-19 11:16:32 -0500

    Method that takes a single session_id string argument             and returns the corresponding User or None

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -62,3 +62,14 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=_generate_uuid())
         return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Method that takes a single session_id string argument
+            and returns the corresponding User or None """
+        if session_id is None:
+            return None
+        user_found = self._db.find_user_by(session_id=session_id)
+        if user_found:
+            return user_found
+        else:
+            return None
