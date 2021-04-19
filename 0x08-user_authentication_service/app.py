d950b15062b3891e3a23664b059e7b5a4646859d
kimha1030 <1478@holbertonschool.com>
2021-04-19 11:06:52 -0500

    Add method login that allow create a new session and store its session_id as cookie, otherwise, the response is abort(401)

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,20 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ Method login """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response_session = jsonify({"email": email, "message": "logged in"})
+        response_session.set_cookie("session_id", session_id)
+        return response_session
 
 
 if __name__ == "__main__":
