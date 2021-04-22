222ba8047f5e689a81c1974f3b8f39beead24168
kimha1030 <1478@holbertonschool.com>
2021-04-22 18:51:32 -0500

    Add babel config file, translations and template

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Task 3. Parametrize templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+import os
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("3-app.Config")
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome_msg():
+    """ Method welcome_msg """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ Method get_locale """
+    lang = app.config.get("LANGUAGES")
+    return request.accept_languages.best_match(lang)
+
+
+if __name__ == "__main__":
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
