5ee79830987efe041cd55e5f951f226e0decc31f
kimha1030 <1478@holbertonschool.com>
2021-04-22 16:27:05 -0500

    Add parameter in class

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config():
+class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
