5e745817c6b98480b566c6b6c0f28a7592478e61
kimha1030 <1478@holbertonschool.com>
2021-04-27 23:42:17 -0500

    Add methods get and get_str

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,3 +20,19 @@ class Cache:
         r_key = str(uuid4())
         self._redis.set(r_key, data)
         return r_key
+
+    def get(self, key: str, fn: typing.Optional[typing.Callable] =
+            None) -> typing.Union[str, bytes, int, float]:
+        """ method get that take a key str argument and optional callable """
+        value = self._redis.get(key)
+        if fn:
+            return (fn(value))
+        else:
+            return value
+
+    def get_str(self, key: str) -> str:
+        """ method that will automatically parametrize Cache.get with the
+            correct conversion function """
+        value = self._redis.get(key)
+        val_dec = value.decode("utf-8")
+        return val_dec
