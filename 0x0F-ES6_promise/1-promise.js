61a49f0969e5721c8716732c3117bd9f0238917c
kimha1030 <1478@holbertonschool.com>
2021-05-15 11:16:35 -0500

    Change syntax of promise

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getFullResponseFromAPI(sucess) {
-  const promise = new Promise((resolve, reject) => {
-    if (success) {
+  return new Promise((resolve, reject) => {
+    if (sucess) {
       resolve({ status: 200, body: "Success" });
     } else {
       reject(new Error("The fake API is not working currently"));
     }
   });
-  return promise;
 }
