fd49ce962a6cde0bcd24e342b4042ee4840cd754
kimha1030 <1478@holbertonschool.com>
2021-05-16 22:55:41 -0500

    Task 8 Throw error - try/catch

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -2,6 +2,6 @@ export default function divideFunction(numerator, denominator) {
   if (denominator == 0) {
     throw Error('cannot divide by 0');
   }
-  const op_divide = numerator / denominator;
-  return op_divide;
+
+  return numerator / denominator;
 }
