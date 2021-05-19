06369f6f808961e385716fa2711453b609197dd8
kimha1030 <1478@holbertonschool.com>
2021-05-18 23:28:08 -0500

    0. Basic list of objects

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const array_objs = [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Colombia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+  return array_objs;
+}
