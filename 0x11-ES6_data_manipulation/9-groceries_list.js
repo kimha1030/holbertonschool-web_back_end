a2d5832a1cef5f0f9c9659f42db75edba3b30fe2
kimha1030 <1478@holbertonschool.com>
2021-05-18 23:54:19 -0500

    Fix quantity

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -2,8 +2,8 @@ export default function groceriesList() {
   const newMap = new Map();
   newMap.set('Apples', 10);
   newMap.set('Tomatoes', 10);
-  newMap.set('Pasta', 10);
-  newMap.set('Rice', 10);
-  newMap.set('Banana', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
   return newMap;
 }
