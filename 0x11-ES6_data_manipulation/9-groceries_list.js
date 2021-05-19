ed96685d5e7db5af0e187ebecf826acc32f9c53f
kimha1030 <1478@holbertonschool.com>
2021-05-18 23:52:08 -0500

    Task 9 Map data structure

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const newMap = new Map();
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 10);
+  newMap.set('Rice', 10);
+  newMap.set('Banana', 10);
+  return newMap;
+}
