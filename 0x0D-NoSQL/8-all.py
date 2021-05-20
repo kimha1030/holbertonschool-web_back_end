9f6b53c479bbb31feb14be5192386babee97bfae
kimha1030 <1478@holbertonschool.com>
2021-05-20 18:29:15 -0500

    Function that list all docs in a collection

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,15 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Task 8. List all documents in Python
 """
-
-import pymongo
 
 
 def list_all(mongo_collection):
     """Function that lists all documents in a collection"""
     list_doc = mongo_collection.find()
-    if list_doc == []:
+    if list_doc.count() == 0:
         return []
     else:
         return list_doc
