413705109a4a08b97429428d064b0a62158e6fae
kimha1030 <1478@holbertonschool.com>
2021-05-20 19:19:33 -0500

    function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity (arrayObjs, city, newGrades) {
+  const filterStudents = arrayObjs.filter((i) => i.location === city);
+  return filterStudents.map((i) => {
+    const newGrade = newGrades.filter((j) => j.studentId === i.id);
+    let grade;
+    if (newGrade[0]) {
+      grade = newGrade[0].grade;
+    } else {
+      grade = 'N/A';
+    }
+    return {
+      ...i,
+      grade
+    };
+  });
+}
