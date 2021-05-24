02e6e61f220f1a12c41f7a1186d62e80e0b2ec8c
kimha1030 <1478@holbertonschool.com>
2021-05-24 17:28:48 -0500

    Show message using process stdin and stdout

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const userInput = process.stdin.read();
+  if (userInput) {
+    process.stdout.write(`Your name is: ${userInput}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
