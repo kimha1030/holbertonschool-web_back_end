a6c62de2375ebe68b94808f26c1d2446bc66dc75
kimha1030 <1478@holbertonschool.com>
2021-05-25 10:10:02 -0500

    Function that accept as argument a csv and shows the number the students by fields and the names

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+module.exports = function countStudents(pathArg) {
+  try {
+    const info = fs.readFileSync(pathArg, 'utf-8');
+    const strLine = info.toString().split('\n');
+    const studList = {};
+    for (let i = 1; i < strLine.length - 1; i++) {
+      const word = strLine[i].split(',');
+      if (studList[word[3]]) {
+        studList[word[3]].numStudents = studList[word[3]].numStudents + 1;
+        studList[word[3]].nameStudents.push(` ${word[0]}`);
+      } else {
+        studList[word[3]] = { numStudents: 1, nameStudents: [`${word[0]}`] };
+      }
+    }
+    const totalStudents = strLine.length - 2; //Menos encabezado y ultima fila
+    console.log(`Number of students: ${totalStudents}`);
+    for (const s in studList) {
+      console.log(
+        `Number of students in ${s}: ${studList[s].numStudents}. List: ${studList[s].nameStudents}`
+      );
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
