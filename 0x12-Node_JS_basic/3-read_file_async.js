0dc56d2f1776bded06bcf45ac5ce3c08a5c217fa
kimha1030 <1478@holbertonschool.com>
2021-05-25 11:39:27 -0500

    Add conditional if data exists

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -6,24 +6,26 @@ function countStudents(pathArg) {
       if (e) {
         reject(Error('Cannot load the database'));
       }
-      const strLine = data.toString().split('\n');
-      const studList = {};
-      for (let i = 1; i < strLine.length - 1; i += 1) {
-        const word = strLine[i].split(',');
-        if (studList[word[3]]) {
-          studList[word[3]].numStudents += 1;
-          studList[word[3]].nameStudents.push(` ${word[0]}`);
-        } else {
-          studList[word[3]] = { numStudents: 1, nameStudents: [`${word[0]}`] };
+      if (data) {
+        const strLine = data.toString().split('\n');
+        const studList = {};
+        for (let i = 1; i < strLine.length - 1; i += 1) {
+          const word = strLine[i].split(',');
+          if (studList[word[3]]) {
+            studList[word[3]].numStudents += 1;
+            studList[word[3]].nameStudents.push(` ${word[0]}`);
+          } else {
+            studList[word[3]] = { numStudents: 1, nameStudents: [`${word[0]}`] };
+          }
         }
-      }
-      const totalStudents = strLine.length - 2; // Menos encabezado y ultima fila
-      console.log(`Number of students: ${totalStudents}`);
-      for (const s in studList) {
-        if (Object.prototype.hasOwnProperty.call(studList, s)) {
-          console.log(
-            `Number of students in ${s}: ${studList[s].numStudents}. List: ${studList[s].nameStudents}`,
-          );
+        const totalStudents = strLine.length - 2; // Menos encabezado y ultima fila
+        console.log(`Number of students: ${totalStudents}`);
+        for (const s in studList) {
+          if (Object.prototype.hasOwnProperty.call(studList, s)) {
+            console.log(
+              `Number of students in ${s}: ${studList[s].numStudents}. List: ${studList[s].nameStudents}`,
+            );
+          }
         }
       }
       resolve();
