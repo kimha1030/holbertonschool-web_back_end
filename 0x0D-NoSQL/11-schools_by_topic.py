d79d7169e32fc9d07b217625a5c1fc05fe33009f
kimha1030 <1478@holbertonschool.com>
2021-09-07 22:52:53 -0500

    Returns the list of school having a specific topic

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -7,5 +7,5 @@ Task 11. Returns the list of school having a specific topic
 def schools_by_topic(mongo_collection, topic):
     """Function that returns the list of school having a
        specific topic"""
-    find_topic = mongo_collection.find({"topic": topic})
+    find_topic = mongo_collection.find({"topics": topic})
     return find_topic
