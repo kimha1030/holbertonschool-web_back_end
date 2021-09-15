910f06fa9cb52da735dd308f224c1eec08c46016
kimha1030 <1478@holbertonschool.com>
2021-09-14 21:03:39 -0500

    Task 3. Handle multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
