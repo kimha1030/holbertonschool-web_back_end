6a42317ee5472bd12deeb93774c34ec64a2cf6da
kimha1030 <1478@holbertonschool.com>
2021-09-14 22:22:21 -0500

    Task 6. Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const prom1 = signUpUser(firstName, lastName);
   const prom2 = uploadPhoto(fileName);
   const elems = [prom1, prom2];
-  return Promise.allSettled(elems)
-    .then((response) => response);
+  const response = await Promise.allSettled(elems);
+  return response;
 }
