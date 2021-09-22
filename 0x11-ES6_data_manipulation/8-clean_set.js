47b75682925a112d814806cb7e26fae510f5f55b
kimha1030 <1478@holbertonschool.com>
2021-09-21 23:12:35 -0500

    function named cleanSet that returns a string of all the set values that start with a specific string

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
-export default function cleanSet (set, startString) {
+export default function cleanSet(set, startString) {
   let valStr = '';
   if (startString === '') {
     return valStr;
-  } else {
-    set.forEach((i) => {
-      if (i.includes(startString)) {
-        valStr = valStr.concat(`-${i.replace(startString, '')}`);
-      }
-    });
-    valStr = valStr.substring(1);
   }
+  set.forEach((i) => {
+    if (i.includes(startString)) {
+      valStr = valStr.concat(`-${i.replace(startString, '')}`);
+    }
+  });
+  valStr = valStr.substring(1);
+
   return valStr;
 }
