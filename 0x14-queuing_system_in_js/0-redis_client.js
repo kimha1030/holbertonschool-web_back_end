9a130eb371b5ea62ce4a697fa1f80caf6c5da6ce
kimha1030 <1478@holbertonschool.com>
2021-10-05 21:20:20 -0500

    Node Redis Client

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -1,3 +1,4 @@
+import redis from 'redis';
 import { createClient } from 'redis';
 
 const client = createClient();
@@ -5,5 +6,5 @@ client.on('connect', () => {
     console.log('Redis client connected to the server')
 });
 client.on('error', (err) => {
-    console.log('Redis client connected to the server:', err)
+    console.log('Redis client not connected to the server:', err)
 });
