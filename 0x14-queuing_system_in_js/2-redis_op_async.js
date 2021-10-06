d0684ae886deebcdc5dcce8b2ce0ac1465503240
kimha1030 <1478@holbertonschool.com>
2021-10-05 22:10:03 -0500

    Node Redis client and async operations

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,25 @@
+import redis from 'redis';
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+const prom = promisify(client.get).bind(client);
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server:', err)
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print)
+}
+
+function displaySchoolValue(schoolName) {
+    const res = await prom(schoolName);
+    console.log(res);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
