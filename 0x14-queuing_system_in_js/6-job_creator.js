d744e2d96c23e4b07389b1ea95206af7c5bcceec
kimha1030 <1478@holbertonschool.com>
2021-10-12 18:08:36 -0500

    Task 6. Create the Job creator

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -1,18 +1,21 @@
 import kue from 'kue';
 
 const queue = kue.createQueue();
-const job = queue.create({
+// datos de ejm en intranet
+const j = ({   
     phoneNumber: '4153518780',
     message: 'This is the code to verify your account'
 });
-const queuenot = 'push_notification_code';
-const jobnot = queue.create(queuenot, job).save((err) => {
-    if (!err) console.log(`Notification job created: ${job.id}`);
+const q = 'push_notification_code';
+const jobnot = queue.create(q, j).save((err) => {
+    if (!err) {
+        console.log(`Notification job created: ${jobnot.id}`);
+    }
 });
-jon.on('completed', () => {
+jobnot.on('complete', () => {
     console.log('Notification job completed');
 });
 
-jon.on('failed', (err) => {
+jobnot.on('failed', (err) => {
     console.log('Notification job failed');
 });
