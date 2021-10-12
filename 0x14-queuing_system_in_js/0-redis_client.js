49bec8742e8522c034115ee3d43329ae4b193c41
kimha1030 <1478@holbertonschool.com>
2021-10-12 18:00:56 -0500

    Script that should connect to the Redis server and show messages(connect, error)

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,9 @@
-import redis from 'redis';
 import { createClient } from 'redis';
 
 const client = createClient();
 client.on('connect', () => {
-    console.log('Redis client connected to the server')
+    console.log('Redis client connected to the server');
 });
 client.on('error', (err) => {
-    console.log('Redis client not connected to the server:', err)
+    console.log('Redis client not connected to the server:', err);
 });
