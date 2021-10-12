d73da5b12a0154fa5d96423bff3239309b81c247
kimha1030 <1478@holbertonschool.com>
2021-10-12 18:06:17 -0500

    Task 3: use the client to store a hash value

--- 0x14-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,21 +1,20 @@
-import { createClient } from 'redis';
+import { createClient, print } from 'redis';
 
 const client = createClient();
-
 client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
-
 client.on('error', (err) => {
     console.log('Redis client not connected to the server:', err);
 });
 
-client.hSet(['HolbertonSchools', 'Portland', '50'], redis.print);
-client.hSet(['HolbertonSchools', 'Seattle', '80'], redis.print);
-client.hSet(['HolbertonSchools', 'New York', '20'], redis.print);
-client.hSet(['HolbertonSchools', 'Bogota', '20'], redis.print);
-client.hSet(['HolbertonSchools', 'Cali', '40'], redis.print);
-client.hSet(['HolbertonSchools', 'Paris', '2'], redis.print);
+client.hSet(['HolbertonSchools', 'Portland', '50'], print);
+client.hSet(['HolbertonSchools', 'Seattle', '80'], print);
+client.hSet(['HolbertonSchools', 'New York', '20'], print);
+client.hSet(['HolbertonSchools', 'Bogota', '20'], print);
+client.hSet(['HolbertonSchools', 'Cali', '40'], print);
+client.hSet(['HolbertonSchools', 'Paris', '2'], print);
+
 client.hGetAll('HolbertonSchools', (err, response) => {
     console.log(response);
 });
